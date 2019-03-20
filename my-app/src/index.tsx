4196004405b57605386649323289751346978d59
Lyubomir Popov <lhpopov@yahoo.com>
2019-03-20 17:20:30 +0200

    Added first example. Added some Interfaces, types and reducers.

---
+++ my-app/src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import Hello from './components/Hello';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+
+
+ReactDOM.render(
+  <Hello name="First test" enthusiasmLevel={10} />,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
