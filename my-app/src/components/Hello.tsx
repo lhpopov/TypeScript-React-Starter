ce1b07a343bd5a2b05b36c70980d773b52abc40c
Lyubomir Popov <lhpopov@yahoo.com>
2019-03-21 10:11:29 +0200

    Added a container, reducer, type and a test for Hello component. Added redux store through a Provider component.

--- my-app/src/components/Hello.tsx
+++ my-app/src/components/Hello.tsx
@@ -3,43 +3,27 @@ import './css/Hello.css';
 
 export interface Props {
     name: string,
-    enthusiasmLevel? : number
+    enthusiasmLevel? : number,
+    onIncrement?: () => void,
+    onDecrement?: () => void
 }
 
-interface State {
-    currentEnthusiasm: number;
-}
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic. :D');
+  }
 
-class Hello extends React.Component < Props, State > {
-    constructor(props: Props) {
-        super(props);
-        this.state = { currentEnthusiasm: (typeof props.enthusiasmLevel !== 'undefined' ? props.enthusiasmLevel : 1) };
-    }
-
-    onIncrement = () => { this.updateEnthusiasm(this.state.currentEnthusiasm + 1)};
-    onDecrement = () => { this.updateEnthusiasm(this.state.currentEnthusiasm - 1)};
-
-    updateEnthusiasm(currentEnthusiasm: number){
-        this.setState({ currentEnthusiasm });
-    }
-
-    render() {
-        const { name } = this.props;
-
-        if (this.state.currentEnthusiasm <= 0) {
-            throw new Error('You could be a little more enthusiastic. :D');
-        }
-
-        return (
-            <div className="hello">
-                <div className="greeting">
-                Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
-                </div>
-                <button onClick={ this.onIncrement }>+</button>
-                <button onClick={ this.onDecrement }>-</button>
-            </div>
-        );
-    }
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
+      </div>
+    </div>
+  );
 }
 
 export default Hello;
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import Hello from './components/Hello';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-
+const store = createStore<StoreState, any, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <Hello name="First test" enthusiasmLevel={10} />,
+	<Provider store={store}>
+  		<Hello />
+	</Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
